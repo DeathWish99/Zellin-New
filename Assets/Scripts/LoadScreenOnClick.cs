37fd6b648063638241c4b85d2d09bbe1b9853eab
Minhuei <chang.minhuei@gmail.com>
2019-01-16 08:43:33 +0700

    c

--- Assets/Scripts/LoadScreenOnClick.cs
+++ Assets/Scripts/LoadScreenOnClick.cs
@@ -2,24 +2,40 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
+
 public class LoadScreenOnClick : MonoBehaviour {
-    public int currScene = 2;
-        //SceneManager.GetActiveScene().buildIndex;
+    public int lastscene;
+    public int currScene;
+    private int count=1;
+    //SceneManager.GetActiveScene().buildIndex;
     public void LoadByIndex(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
     }
-    public void LoadBycurr(int indexscene)
+
+    private LoadScreenOnClick ()
     {
+
+    }
+    public void Loadbycurr ()
+    {
+        currScene = lastscene;
         //SceneManager.LoadScene(currScene);
-        if (currScene != 6)
+
+        if (currScene>1 && currScene != 6)
         {
-            SceneManager.LoadScene(currScene=currScene + 1);
+            SceneManager.LoadScene(currScene+=count);
+            lastscene = currScene;
+            
+            count++;
+            
         }
         else
         {
-            SceneManager.LoadScene(currScene=currScene + 2);
+            SceneManager.LoadScene(currScene+=count+1);
+            lastscene = currScene;
+            count++;
+           
         }
     }
-
 }
